e56f2f38fe59fa4e9bd804f7bf587ff3183e630e
Nataly Rybak <natalya.rybak@gmail.com>
2018-10-28 06:15:25 +0200

    Initial commit

---
+++ WalletSimulator/ViewModels/Editor/EditorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMA.APZRPMJ2018.WalletSimulator.ViewModels.Editor
+{
+    class EditorViewModel
+    {
+    }
+}
