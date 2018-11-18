549b8afe23fc4404a225008ce62aa85a9a83266d
Nataly Rybak <natalya.rybak@gmail.com>
2018-11-18 03:13:02 +0200

    add entity framework

---
+++ TextEditor/Configuration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMA.APZRPMJ2018.TextEditor
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<EditorDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EditorDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ TextEditor/EditorDBContext.cs
@@ -0,0 +1,27 @@
+﻿using KMA.APZRPMJ2018.TextEditor.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMA.APZRPMJ2018.TextEditor
+{
+    internal class EditorDBContext : DbContext
+    {
+        public EditorDBContext() : base("NewEditorDB")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<EditorDBContext, Configuration>(true));
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Query> Queries { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+           // modelBuilder.Configurations.Add(new User.UserEntityConfiguration());
+            //modelBuilder.Configurations.Add(new Query.QueryEntityConfiguration());
+        }
+    }
+}
